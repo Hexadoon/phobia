d83a6a5411a39a735ac71936977cc0764d682a59
Brandon Kwintner <kwintner@terpmail.umd.edu>
2020-06-08 13:56:06 -0400

    Added character animation

--- character/KinematicBody2D.gd
+++ character/KinematicBody2D.gd
@@ -6,28 +6,93 @@ const GRAVITY = 30
 const JUMP_FORCE = -400
 
 var motion = Vector2()
+var idle_animation = "Idle"
 
 func _physics_process(delta):
 	"""
-	This function runs 60 times / second (every frame)
-	Monitors game, checks to see if user presses button,
-	controls movement.
+	Constantly checks to see if user presses button, controls movement.
+	@input: up / w: Jump
+	@input: left / a: Move Left
+	@input: right / d: Move Right
+	@input: down / s: Crouch
+	@input: z: Pick Up
 	"""
-	motion.y += GRAVITY
-	
-	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
-	else:
-		motion.x = 0
-		
-	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
-		motion.y = JUMP_FORCE
+	# On floor.
+	if is_on_floor():
+		# Crouching
+		if Input.is_action_pressed("ui_down"):
+			# Transitions.
+			if Input.is_action_just_pressed("ui_right"):
+				$Sprite.flip_h = false
+				$Sprite.play("Crouch_Transition")
+				
+			elif Input.is_action_just_pressed("ui_left"):
+				$Sprite.flip_h = true
+				$Sprite.play("Crouch_Transition")
 			
-	# Performs the motion (changes x,y coords of vector)
+			# Looping animations.
+			if Input.is_action_pressed("ui_right"):
+				motion.x = SPEED
+				$Sprite.play("Crouch_Walk")
+	
+			elif Input.is_action_pressed("ui_left"):
+				motion.x = -SPEED
+				$Sprite.play("Crouch_Walk")
+			
+			else:
+				motion.x = 0
+				$Sprite.play("Crouch_Idle")
+			
+		# Standing
+		else:
+			# Transitions.
+			if Input.is_action_just_pressed("ui_right"):
+				$Sprite.flip_h = false
+				$Sprite.play("Idle_to_Walk")
+				
+			elif Input.is_action_just_pressed("ui_left"):
+				$Sprite.flip_h = true
+				$Sprite.play("Idle_to_Walk")
+				
+			# Looping animations.
+			if Input.is_action_pressed("ui_right"):
+				motion.x = SPEED
+				$Sprite.play("Walk")
+	
+			elif Input.is_action_pressed("ui_left"):
+				motion.x = -SPEED
+				$Sprite.play("Walk")
+				
+			elif Input.is_action_pressed("ui_pickup"):
+				$Sprite.play("Pick_Up")
+			
+			else:
+				motion.x = 0
+				$Sprite.play(idle_animation)
+
+		if Input.is_action_just_pressed("ui_up"):
+			#TODO add jump animation?
+			motion.y = JUMP_FORCE
+	
+	# In air.
+	else:
+		if Input.is_action_pressed("ui_right"):
+			motion.x = SPEED
+			$Sprite.flip_h = false
+		elif Input.is_action_pressed("ui_left"):
+			motion.x = -SPEED
+			$Sprite.flip_h = true
+
+	motion.y += GRAVITY
 	motion = move_and_slide(motion, UP)
 	
-	
-	
-	
+func _on_Sprite_animation_finished():
+	"""
+	Controls switching between idle and idle blinking animations.
+	"""
+	if idle_animation == "Idle":
+		idle_animation = "Idle_Blinking"
+	elif idle_animation == "Idle_Blinking":
+		idle_animation = "Idle_Looking_Around"
+	else:
+		idle_animation = "Idle"
