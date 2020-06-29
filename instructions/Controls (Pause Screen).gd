625603c0b562a90d2fbcda53df6eaf5fe1f45543
JLin555 <joshua.lin@nyu.edu>
2020-06-28 17:21:27 -0700

    Fixed pause menu bug

--- instructions/Controls (Pause Screen).gd
+++ instructions/Controls (Pause Screen).gd
@@ -1,4 +1,11 @@
-extends Node2D
+extends Control
 
-func _on_Button_pressed():
-	get_tree().change_scene("res://Levels/Level_1/Main.tscn")
+var pause_screen
+
+func _ready():
+	pause_screen = get_parent().get_parent().get_node("CanvasLayer/Pause Screen")
+
+func _on_Back_Button_pressed():
+	get_tree().paused = not get_tree().paused
+	visible = not visible
+	pause_screen.visible = not visible
--- pause_screen/Pause Screen.gd
+++ pause_screen/Pause Screen.gd
@@ -1,5 +1,10 @@
 extends Control
 
+var controls_scene
+
+func _ready():
+	controls_scene = get_parent().get_parent().get_node("CanvasLayer2/Controls (Pause Screen)")
+
 # Reveals pause screen when "escape" is pressed
 # Pauses the game
 func _input(event):
@@ -16,7 +21,8 @@ func _on_Resume_Button_pressed():
 # Unpauses game and changes scene to controls scene
 func _on_Controls_Button_pressed():
 	get_tree().paused = not get_tree().paused
-	get_tree().change_scene("res://instructions/Controls (Pause Screen).tscn")
+	visible = not visible
+	controls_scene.visible = not visible
 
 # Unpauses game and changes scene to main menu scene
 func _on_Exit_To_Menu_Button_pressed():
